0cba37651ce163b4b91fa5c1f5d727c1d0ee9db2
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2016-07-16 18:40:22 -0300

    HTOP and Google Chrome

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -13,9 +13,12 @@ sudo apt-get install -y \
 		curl \
 		bash-completion \
 		nmap \
-		nano
+		nano \
+		htop
 case "$1" in
 	gui)
+	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+	sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 	sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 	sudo add-apt-repository -y ppa:numix/ppa
 	sudo apt-get update
@@ -29,7 +32,8 @@ case "$1" in
 			sublime-text-installer \
 			unity-tweak-tool \
 			numix-gtk-theme \
-			numix-icon-theme-circle
+			numix-icon-theme-circle \
+			google-chrome-stable
 	;;
 	*)
 	;;
