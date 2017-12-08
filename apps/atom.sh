5ce4331489cb9f7b35c24b997c201663357b0b73
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2017-12-07 23:38:16 -0200

    Separated external apps

---
+++ apps/atom.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -xe
+
+sudo add-apt-repository -y ppa:webupd8team/atom
+sudo apt-get update
+sudo apt-get install -y atom
--- mint/install.sh
+++ mint/install.sh
@@ -12,50 +12,28 @@ sudo apt-get install -y \
 		tree \
 		curl \
 		bash-completion \
-		htop
-
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-sudo add-apt-repository -y ppa:webupd8team/atom
-sudo add-apt-repository -y ppa:numix/ppa
-sudo apt-get update
-sudo apt-get install -y \
+		htop \
 		terminator \
 		vlc \
-		ubuntu-wallpaper* \
 		meld \
 		git-cola \
-		atom \
-		numix-gtk-theme \
-		numix-icon-theme-circle \
-		google-chrome-stable \
-		cabextract \
 		synaptic \
 		lxc \
 		lxc-template \
 		redshift \
 		redshift-gtk
 
-mkdir tmp
-cd tmp
-wget http://download.microsoft.com/download/E/6/7/E675FFFC-2A6D-4AB0-B3EB-27C9F8C8F696/PowerPointViewer.exe
-cabextract -L -F ppviewer.cab PowerPointViewer.exe
-cabextract ppviewer.cab
-if [ ! -d ~/.fonts ]; then
-	mkdir ~/.fonts
-fi
-cp *.TT* ~/.fonts
-cd ..
-rm -R tmp
+cd ../apps
+./atom.sh
+./chrome.sh
+./consolas.sh
+./numix.sh
+./spotify.sh
+./vundle.sh
 
-git config --global user.email "pedrobmarin@gmail.com"
-git config --global user.name "Pedro Beschorner Marin"
-
-if [ ! -d ~/.vim/bundle/vundle ]; then
-	git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-fi
 cd ../config
 cp vim ~/.vimrc
 cp tmux ~/.tmux.conf
+cp git ~/.gitconfig
 mkdir -p ~./config/terminator
 cp terminator ~/.config/terminator/config
