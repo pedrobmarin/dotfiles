4fd537d81a4eed32d47a1a662dd5219b6eb725c8
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2017-12-09 16:39:09 -0200

    Updating Ubuntu install script

--- distros/ubuntu/install.sh
+++ distros/ubuntu/install.sh
@@ -2,6 +2,12 @@
 
 set -xe
 
+APPS=../../apps
+CONFIGS=../../configs
+THEMES=../../themes
+FONTS=../../fonts
+PLUGINS=../../plugins
+
 sudo apt-get update
 sudo apt-get install -y \
 		wget \
@@ -12,60 +18,28 @@ sudo apt-get install -y \
 		tree \
 		curl \
 		bash-completion \
-		nmap \
-		nano \
-		htop
-case "$1" in
-	gui)
-	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-	sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-	sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
-	sudo add-apt-repository -y ppa:numix/ppa
-	sudo apt-get update
-	sudo apt-get install -y \
-			terminator \
-			vlc \
-			ubuntu-wallpaper* \
-			meld \
-			git-cola \
-			sublime-text-installer \
-			unity-tweak-tool \
-			numix-gtk-theme \
-			numix-icon-theme-circle \
-			google-chrome-stable \
-			cabextract \
-			synaptic \
-			lxc
-	mkdir tmp
-	cd tmp
-	wget http://download.microsoft.com/download/E/6/7/E675FFFC-2A6D-4AB0-B3EB-27C9F8C8F696/PowerPointViewer.exe
-	cabextract -L -F ppviewer.cab PowerPointViewer.exe
-	cabextract ppviewer.cab
-	if [ ! -d ~/.fonts ]; then
-		mkdir ~/.fonts
-	fi
-	cp *.TT* ~/.fonts
-	cd ..
-	rm -R tmp
-	;;
-	*)
-	;;
-esac
+		htop \
+		terminator \
+		vlc \
+		ubuntu-wallpaper \
+		unity-tweak-tool \
+		meld \
+		git-cola \
+		synaptic \
+		lxc \
+		redshift \
+		redshift-gtk
 
-git config --global user.email "pedrobmarin@gmail.com"
-git config --global user.name "Pedro Beschorner Marin"
+./$APPS/atom.sh
+./$APPS/chrome.sh
+./$APPS/spotify.sh
+./$FONTS/consolas.sh
+./$THEMES/numix.sh
+./$PLUGINS/vundle.sh
+./$PLUGINS/tpm.sh
 
-if [ ! -d ~/.vim/bundle/vundle ]; then
-	git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-fi
-cd ../config
-cp vim ~/.vimrc
-cp tmux ~/.tmux.conf
-case "$1" in
-	gui)
-	mkdir -p ~./config/terminator
-	cp terminator ~/.config/terminator/config
-	;;
-	*)
-	;;
-esac
+cp $CONFIGS/vim ~/.vimrc
+cp $CONFIGS/tmux ~/.tmux.conf
+cp $CONFIGS/git ~/.gitconfig
+mkdir -p ~./config/terminator
+cp $CONFIGS/terminator ~/.config/terminator/config
